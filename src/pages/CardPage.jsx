e59f46e7b1ea35c35c0ecef249ff53c49d2079ad
Sk Masum <masum0aktar@gmail.com>
2023-11-04 15:12:21 +0530

    first commit

---
+++ src/pages/CardPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const CardPage = () => {
+  return (
+
+    
+    <>
+    <div>CardPage</div>
+    <div>CardPage</div>
+    <div>CardPage</div>
+    <div>CardPage</div>
+    <div>CardPage</div>
+    <div>CardPage</div>
+    <div>CardPage</div>
+    </>
+  )
+}
+
+export default CardPage;
